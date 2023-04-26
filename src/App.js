28c70239b23447c978282d160f6ab35bf9857cd2
TheTagadaStrawberry <reveillere.julien@outlook.fr>
2023-04-26 13:46:45 +0200

    m

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Accueil from './accueil/Accueil';
 import Actualite from './actualites/actualite';
 import Actualites from './actualites/actualites';
 import Inscription from './contact/inscription';
-import NousRetrouver from './club/nousRetrouver';
+import Acces from './club/acces';
 
 import './App.css';
 
@@ -19,12 +19,11 @@ function App() {
     <>
       <Header />
       <Routes>
-        {/* <Route path="/" element={<Accueil />} />  */}
         <Route path="ATT-ST-Antoine" element={<Accueil />} />
         <Route path="ATT-ST-Antoine/actualite" element={<Actualite />} />
         <Route path="ATT-ST-Antoine/actualites" element={<Actualites />} />  
         <Route path="ATT-ST-Antoine/club" element={<Club />} />
-        <Route Path="ATT-ST-Antoine/plan-d-acces" element={<NousRetrouver />} />
+        <Route Path="ATT-ST-Antoine/plan-d-acces" element={<Acces />} />
         <Route path="ATT-ST-Antoine/competitions" element={<Competition />} />
         <Route path="ATT-ST-Antoine/inscription" element={<Inscription />} />
         <Route path="ATT-ST-Antoine/formulaire" element={<Contact />} />
